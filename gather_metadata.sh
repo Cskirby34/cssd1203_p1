b63aa60d975536be630a307570f8ebd108ffd021
Cskirby34 <cmking3388@gmail.com>
2026-09-20 10:40:32 -0400

    Update gather_metadata.sh

--- gather_metadata.sh
+++ gather_metadata.sh
@@ -1,1 +1,21 @@
 #! /usr/bin/env bash
+
+if ["$#" -gt 1]; then
+  exit 1
+fi
+
+if [ "$#" -eq 0 ]; then
+    dir="."
+else
+    dir="$1"
+fi
+
+if [ ! -d "$dir" ]; then
+    printf '%s is not a directory.\n' "$dir" >&2
+    exit 1
+fi
+
+if [ ! -r "$dir" ] || [ ! -x "$dir" ]; then
+    printf '%s is not readable or executable.\n' "$dir" >&2
+    exit 1
+fi
